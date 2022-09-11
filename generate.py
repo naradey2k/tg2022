24b2ea2587904d9826c5e063f4317fffe43a2e83
doksterium <56970175+naradey2k@users.noreply.github.com>
2022-09-11 21:07:35 +0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,13 +2,13 @@ from main import *
 import argparse
 import numpy as np
 
-gm = GenModel('/content/drive/MyDrive/philosophy.txt', 'gg.pth')
+gm = GenModel('data/philosophy.txt', 'gg.pth')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str)
 parser.add_argument('--prefix', type=str, default=np.random.choice(gm.clean_text.split(), 1))
 parser.add_argument('--length', type=int)
-args = parser.parse_args()
+args = parser.parse_args())
 
 pre = gm.generate(args.model, args.prefix, args.length)
 print(pre)
