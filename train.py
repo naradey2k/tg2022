e7e5d923438c434adac94bbe78f8fd99e74cf718
doksterium <56970175+naradey2k@users.noreply.github.com>
2022-09-11 21:08:18 +0600

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from main import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input-dir', type=str)
+parser.add_argument('--input-dir', type=str, default='data/philosophy.txt')
 parser.add_argument('--model', type=str)
 args = parser.parse_args()
 
